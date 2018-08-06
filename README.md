b1f44b3a6ea473526ee93f499f5ea366ec79b3e3
ClobberXD <ClobberXD@gmail.com>
2018-08-06 14:29:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Sniper
+
+Sniper is a Minetest mod which adds advanced sniper rifles to the game. Sniper comes with ~4 unique rifles~, and it also provides a nice API to register custom rifles. It differs from other gun mods in that it's very realistic - rifles can have functional-scopes with zoom, realistic recoil, accurate shoot and reload sounds, etc. These features can also differ from rifle-to-rifle - check out the registration section for more details.
+
+## Registering custom rifles
+
+New sniper rifles can be registered using the `sniper.register_rifle(name, def)` function, where `name` is a string used to address the rifle internally, and `def` is a table containing the rifle's definition. Here's an example def table that provides the possible properties with their default values:
+
+```lua
+{
+    -- The name displayed in the inventory and other places visible to the end user (player).
+    display_name = "Sniper Rifle",
+
+    -- Inventory and wield images
+    inventory_image = "",
+    wield_image = "",
+
+    -- Scope FOV (in degrees) is used to specify the amount of zoom provided by the rifle's scope.
+    -- Valid range of values: 1 to 160
+    scope_fov = 60,
+
+    -- Scope style accepts values from 1 to 4. Style 1 is the simplest, style 4 is the most sophisticated.
+    scope_style = 1,
+
+    -- Damage dealt (in HP) will be calculated using the following formula: "sniper.base_dmg * damage_mult".
+    -- Valid range of values: 0.1 to 5
+    damage_mult = 1.0,
+
+    -- Stability multiplier affects the amount of recoil after every shot.
+    -- Valid range of values: 0.1 to 5
+    stab_mult = 1.0,
+
+    -- Fire rate multiplier is used to calculate the reload time between each shot.
+    -- Valid range of values: 0.1 to 5
+    fire_rate = 1.0
+}
+```
+
+## TODO
+
+- Ability to upgrade. e.g. better scope, improved stability, etc.
+  - Maybe pass another table to `sniper.register_rifle` defining all possible upgrade paths?
+- Support for custom shoot/reload sounds; default to provided sounds.
+
+### Attributions
+
+- Sounds licensed under CC-BY-SA 3.0
+  - [sniper_shoot.ogg](https://freesound.org/people/EMSIarma/sounds/108852/)
+  - [sniper_scope_toggle.ogg](https://freesound.org/people/TicTacShutUp/sounds/406/)
---
+++ api.lua
@@ -0,0 +1,146 @@
+sniper = {}
+sniper.rifles = {}   -- Stores the rifle definition tables
+local scope_hud = {} -- Stores the ID of the scope overlay HUD element
+local hud_flags = {} -- Stores the player's HUD flags before disabling them
+local interval = {}  -- Interval between each shot, depends on rifle's fire-rate
+local base_dmg = 6   -- Base damage, used for calculating damage dealt
+
+minetest.register_on_joinplayer(function(player)
+	scope_hud[player:get_player_name()] = nil
+end)
+
+minetest.register_on_wielditem_change(function(player, old, new)
+	hide_scope(player)
+end)
+
+function show_scope(player, fov, style)
+	if not player then
+		return
+	end
+
+	local name = player:get_player_name()
+
+	-- Disable all default HUD elements
+	hud_flags[name] = player:hud_get_flags()
+	player:hud_set_flags({
+		hotbar    = false,
+		healthbar = false,
+		breathbar = false,
+		crosshair = false,
+		wielditem = false
+	})
+
+	player:set_fov(fov)
+
+	scope_hud[name] = player:hud_add({
+		hud_elem_type = "image",
+		scale = {x = -100, y = -100},
+		position = {x = 0.5, y = 0.5},
+		alignment = {x = 0, y = 0},
+		text = "sniper_scope_style_" .. style .. ".png"
+	})
+end
+
+function hide_scope(player)
+	local name = player:get_player_name()
+	player:clear_fov()
+	player:hud_remove(scope_hud[name])
+	player:hud_set_flags(hud_flags[name])
+	scope_hud[name] = null
+end
+
+function left_click(itemstack, player, pointed_thing)
+	local name = player:get_player_name()
+	local rifle = sniper.rifles[itemstack:get_name()]
+
+	-- Return if player isn't viewing through the scope
+	if not scope_hud[name] then
+		return
+	end
+
+	-- Return if player fires before interval expires
+	if interval[name] and os.time() < interval[name] then
+		return
+	end
+
+	-- Fire!
+
+	-- Calculate and set damage dealt
+	local damage = math.floor(base_dmg * rifle.damage_mult)
+	-- target:set_hp(damage)
+
+	-- Simulate recoil, intensity depends on rifle.stab_mult
+	player:set_look_vertical(-0.005 + (0.001 * rifle.stab_mult))
+
+	-- Set interval, depends on fire_rate
+	-- If fire_rate == 2 shots/sec, interval = os.time() + 0.5s
+	interval[name] = os.time() + (1 / rifle.fire_rate)
+
+	-- Play shot-fired sound
+	minetest.sound_play("sniper_shoot", {to_player = name})
+
+	return itemstack
+end
+
+function right_click(itemstack, player, pointed_thing)
+	local name = player:get_player_name()
+	local rifle = sniper.rifles[itemstack:get_name()]
+
+	-- Play a click sound for indicating scope toggle
+	minetest.sound_play("sniper_scope_toggle", {to_player = name})
+
+	-- Check if player is looking through the scope
+	if not scope_hud[name] then
+		show_scope(player, rifle.scope_fov, rifle.scope_style)
+	else
+		hide_scope(player)
+	end
+
+	return itemstack
+end
+
+-- Verify of given rifle definition table
+function verify_def(def)
+	-- Check for missing fields
+	def.display_name = def.display_name and def.display_name
+		or "Sniper Rifle #" .. (#sniper.rifles + 1)
+	def.scope_fov   = def.scope_fov and def.scope_fov or 60
+	def.scope_style = def.scope_style and def.scope_style or 1
+	def.damage_mult = def.damage_mult and def.damage_mult or 1.0
+	def.stab_mult   = def.stab_mult and def.stab_mult or 1.0
+	def.fire_rate   = def.fire_rate and def.fire_rate or 1.0
+
+	-- Check for invalid field values
+	def.scope_fov = (def.scope_fov >= 20 and def.scope_fov <= 80)
+						and def.scope_fov or 60
+	def.scope_style = (def.scope_style >= 1 and def.scope_style <= 4)
+						and def.scope_style or 1
+	def.damage_mult = (def.damage_mult >= 0.1 and def.damage_mult <= 5)
+						and def.damage_mult or 1.0
+	def.stab_mult = (def.stab_mult >= 0.1 and def.stab_mult <= 5)
+						and def.stab_mult or 1.0
+	def.fire_rate = (def.fire_rate >= 0.5 and def.fire_rate <=5)
+						and def.fire_rate or 1.0
+	return def
+end
+
+function sniper.register_rifle(name, def)
+	if sniper.rifles[name] then
+		minetest.log("warning",
+			"[sniper] Attempting to register new rifle with pre-existing name")
+		return
+	end
+
+	sniper.rifles[name] = verify_def(def)
+
+	minetest.register_tool(name, {
+		description = def.display_name,
+		inventory_image = def.inventory_image,
+		wield_image = def.wield_image,
+		stack_max = 1,
+		range = 0.0,
+
+		on_use = left_click,
+		on_secondary_use = right_click
+	})
+end
